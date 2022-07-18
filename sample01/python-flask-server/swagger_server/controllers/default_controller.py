5f475d9c2ff4931c34d268e48b37518e8868ba77
usamippoi <usamippoi@gmail.com>
2022-07-18 21:48:54 +0900

    Add: 公式Swagger Editorで表示されたサンプルから、 Swagger Codegenを使用してサーバ側コードを自動生成

---
+++ sample01/python-flask-server/swagger_server/controllers/default_controller.py
@@ -0,0 +1,17 @@
+import connexion
+import six
+
+from swagger_server import util
+
+
+def posts_id_get(id):  # noqa: E501
+    """posts_id_get
+
+    description 説明文  # noqa: E501
+
+    :param id: Id of array
+    :type id: float
+
+    :rtype: List[object]
+    """
+    return 'do some magic!'
---
+++ sample01/python-flask-server/swagger_server/test/test_default_controller.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+from flask import json
+from six import BytesIO
+
+from swagger_server.test import BaseTestCase
+
+
+class TestDefaultController(BaseTestCase):
+    """DefaultController integration test stubs"""
+
+    def test_posts_id_get(self):
+        """Test case for posts_id_get
+
+        
+        """
+        response = self.client.open(
+            '/posts/{id}'.format(id=1.2),
+            method='GET')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
